ceabe4eb066e2b8cdafa9fecbfe5960fa4d32ba1
KevinVinsen <kvn1961@gmail.com>
2015-02-05 12:21:18 +0800

    New script layout

--- slurm/gama-magphys-slurm.sh
+++ slurm/gama-magphys-slurm.sh
@@ -7,9 +7,6 @@
 #SBATCH --ntasks=24
 #SBATCH --ntasks-per-node=24
 #SBATCH --nodes=1
+#SBATCH --array=0-1
 
 aprun -n 24 -N 24 $SLURM_SUBMIT_DIR/gama-magphys.sh
-
-date
-wait
-date
--- slurm/gama-magphys.sh
+++ slurm/gama-magphys.sh
@@ -5,7 +5,6 @@ echo "ALPS_APP_PE = $ALPS_APP_PE"
 echo "SLURM_ARRAY_TASK_ID = $SLURM_ARRAY_TASK_ID"
 echo "SLURM_SUBMIT_DIR = $SLURM_SUBMIT_DIR"
 
-SLURM_ARRAY_TASK_ID=0
 dir_id=`printf %06d $((SLURM_ARRAY_TASK_ID * 24 + ALPS_APP_PE))`
 echo "dir_id = $dir_id"
 directory="/group/partner1024/kvinsen/gama-magphys-data/run_02-2015_02/$dir_id"
