02d1ce2725a72257a64c567a815380df62365e7d
KevinVinsen <kvn1961@gmail.com>
2015-02-05 11:37:26 +0800

    New script layout

---
+++ slurm/gama-magphys-slurm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash --login
+
+# SLURM directives
+#
+#SBATCH --job-name=gama-magphys
+#SBATCH --account=partner1024
+#SBATCH --time=18:00:00
+#SBATCH --ntasks=24
+#SBATCH --ntasks-per-node=24
+
+aprun -n 24 -N 24 $SLURM_SUBMIT_DIR/gama-magphys-exe.sh
