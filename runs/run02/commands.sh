de9e920d22ab0ff6107f42928661a64e9d6ae1fd
KevinVinsen <kvn1961@gmail.com>
2015-02-25 17:09:07 +0800

    Storing neds results with cb07

--- runs/run02/commands.sh
+++ runs/run02/commands.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./preprocess_dat_file.py ./magphys/run02/forkevin.csv /tmp/run /group/partner1024/kvinsen/gama-magphys/magphys run02 cb07
+./preprocess_dat_file.py ./runs/run02/forkevin.csv /tmp/run /group/partner1024/kvinsen/gama-magphys/magphys run02 cb07
