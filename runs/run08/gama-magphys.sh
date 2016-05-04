e33fde34d16939bbd861ed12578dfaba7b9acf6a
KevinVinsen <kvn1961@gmail.com>
2016-05-04 15:03:16 +0800

    Correct the slurm scripts

--- runs/run08/gama-magphys.sh
+++ runs/run08/gama-magphys.sh
@@ -8,7 +8,7 @@ echo "SLURM_SUBMIT_DIR = $SLURM_SUBMIT_DIR"
 
 dir_id=`printf %06d $((SLURM_ARRAY_TASK_ID * 24 + ALPS_APP_PE))`
 echo "dir_id = $dir_id"
-directory="/group/partner1024/kvinsen/gama-magphys-data/run_04-2015_02/$dir_id"
+directory="/scratch/pawsey0160/kvinsen/run08/$dir_id"
 echo "directory = $directory"
 if [ -d $directory ]; then
     cd $directory
