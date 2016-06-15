9f74e944027003ac460859301c461ffa64483235
KevinVinsen <kvn1961@gmail.com>
2016-06-15 10:55:42 +0800

    Added runs 10 and 11

---
+++ runs/run10/gama-magphys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# We are one of 24 running on a node as part of an array job
+# Show the environment variables
+echo "ALPS_APP_PE = $ALPS_APP_PE"
+echo "SLURM_ARRAY_TASK_ID = $SLURM_ARRAY_TASK_ID"
+echo "SLURM_SUBMIT_DIR = $SLURM_SUBMIT_DIR"
+
+dir_id=`printf %06d $((SLURM_ARRAY_TASK_ID * 24 + ALPS_APP_PE))`
+echo "dir_id = $dir_id"
+directory="/scratch/pawsey0160/kvinsen/run10/$dir_id"
+echo "directory = $directory"
+if [ -d $directory ]; then
+    cd $directory
+    bash process_data.sh > "$SLURM_SUBMIT_DIR/$dir_id.log"
+else
+    echo "Could not find directory = $directory"
+fi
