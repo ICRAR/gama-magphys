87c9e3c36d5a41a81f7902eb128674cde3e4412e
KevinVinsen <kvn1961@gmail.com>
2017-04-20 16:05:24 +0800

    Correct the array size

---
+++ runs/3dhst/2017_04_20/cosmos/gama-magphys-slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash --login
+# SLURM directives
+#
+#SBATCH --job-name=gama-magphys
+#SBATCH --account=pawsey0160
+#SBATCH --time=03:00:00
+#SBATCH --ntasks=24
+#SBATCH --ntasks-per-node=24
+#SBATCH --nodes=1
+#SBATCH --array=0-18
+
+# with the node=1 this will run 24 versions of gama-magphys on the same one
+aprun -n 24 -N 24 $SLURM_SUBMIT_DIR/gama-magphys.sh
