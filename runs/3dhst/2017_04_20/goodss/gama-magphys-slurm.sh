56e8bff6e5e7bf4adee513b75bbeb180c40d61be
KevinVinsen <kvn1961@gmail.com>
2017-04-21 10:01:25 +0800

    Added the email

--- runs/3dhst/2017_04_20/goodss/gama-magphys-slurm.sh
+++ runs/3dhst/2017_04_20/goodss/gama-magphys-slurm.sh
@@ -8,6 +8,10 @@
 #SBATCH --ntasks-per-node=24
 #SBATCH --nodes=1
 #SBATCH --array=0-127
+#SBATCH --mail-type=ALL
+#SBATCH --mail-type=TIME_LIMIT_90
+#SBATCH --mail-type=ARRAY_TASKS
+#SBATCH --mail-user kevin.vinsen@icrar.org
 
 # with the node=1 this will run 24 versions of gama-magphys on the same one
 aprun -n 24 -N 24 $SLURM_SUBMIT_DIR/gama-magphys.sh
