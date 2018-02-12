23a1c88af6b76792cba1ee26fe26fd74a5d63c11
KevinVinsen <kvn1961@gmail.com>
2018-02-12 13:35:23 +0800

    Only need 62 steps

--- runs/run15_2018_02_12/gama-magphys-slurm.sh
+++ runs/run15_2018_02_12/gama-magphys-slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=03:00:00
 #SBATCH --ntasks-per-node=24
 #SBATCH --nodes=1
-#SBATCH --array=0-127
+#SBATCH --array=0-62
 
 # with the node=1 this will run 24 versions of gama-magphys on the same one
 srun $SLURM_SUBMIT_DIR/gama-magphys.sh
